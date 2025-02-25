49b875be2253b05188a9cf59cdae2ace9d77ce53
Zeeshan <m.zeeshan@techlogix.com>
2025-02-25 21:04:32 +0500

    .

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,19 @@
+import fs from "fs";
+import matter from "gray-matter";
+import { notFound } from "next/navigation";
+
 export default async function Page({
-    params,
-  }: {
-    params: Promise<{ slug: string }>
-  }) {
-    const slug = (await params).slug
-    const filePath = `content/${slug}`
-    return <div>My Post: {slug}</div>
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const filePath = `content/${slug}.md`;
+  if (!fs.existsSync(filePath)){
+    notFound()
   }
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  return <div>My Post: {content}</div>;
+}
