aebdb81e9244327ca7d4f142fe4336ee7e696730
Zeeshan <m.zeeshan@techlogix.com>
2025-03-11 22:24:38 +0500

    Added header to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,18 @@
 import BlogCards from "@/components/blog-cards";
+import { BreadcrumbWithCustomSeparator } from "@/components/bread-crumb";
+import Header from "@/components/Header";
 
 const Blog = () => {
-  return <BlogCards />;
+  return (
+    <>
+      <Header>
+        <h1 className="title font-semibold text-2xl tracking-tighter mt-4">
+          Blog
+        </h1>
+      </Header>
+      <BlogCards />
+    </>
+  );
 };
 
 export default Blog;
