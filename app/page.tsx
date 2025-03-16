5e6c3e444d483a017dcc33f47b2573e509540b9c
Zeeshan <m.zeeshan@techlogix.com>
2025-03-16 11:06:56 +0500

    Added container to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
+import Container from "@/components/container";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-5xl text-foreground font-bold mb-8 animate-pulse">
-          Coming Soon
-        </h1>
-        <p className="text-foreground/70 text-lg mb-8">
-          We are working hard to bring you something amazing. Stay tuned!
-        </p>
-        <Button asChild>
-          <Link href="/blog">Read the Blog</Link>
-        </Button>
-      </div>
+      <Container>
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <h1 className="text-5xl text-foreground font-bold mb-8 animate-pulse">
+            Coming Soon
+          </h1>
+          <p className="text-foreground/70 text-lg mb-8">
+            We are working hard to bring you something amazing. Stay tuned!
+          </p>
+          <Button asChild>
+            <Link href="/blog">Read the Blog</Link>
+          </Button>
+        </div>
+      </Container>
     </>
   );
 }
