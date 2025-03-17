249e5a6c04a102c75ec3e0568af16d13056027a6
Zeeshan <m.zeeshan@techlogix.com>
2025-03-17 10:28:57 +0500

    updated domain

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export const fetchUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://blog-devcodez.vercel.app/api";
+    : "https://www.devcodez.com/api";
 
 type ResponseData = {
   category: string;
